0f6faa1b78a5f2d9a32aa2f0c87dbe0646cc170d
talscully <32487051+talscully@users.noreply.github.com>
2020-11-27 18:05:36 -0500

    Added activities and icons

---
+++ 2020_advent_calendar/randomize.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+list = np.array([
+    "Decorate an unexpected spot.",
+    "Write a letter to your future self.",
+    "Eat breakfast for dinner.",    # visible, not weekend necessary
+    "Make a list of 21 things to do in 2021.",
+    "Share a fun photo from holiday seasons past.",
+    "Pick a Christmas song lyric signature phrase.",
+    "Create a festive cocktail.",   # visible, Friday or Saturday
+    "Send a message of gratitude.",
+    "Make hot cocoa.",
+    "Research charitable donations or volunteering.",
+    "Research future trips.",
+    "Sing a carol.",
+    "Write a Christmas haiku.",
+    "Take a festive photo.",
+])
--- 2020_advent_calendar/script.js
+++ 2020_advent_calendar/script.js
@@ -1,21 +1,111 @@
 function onPageLoad() {
 
-    this_date = new Date()
-    this_day = this_date.getDate()
-    this_month = this_date.getMonth()+1
+    this_date = new Date();
+    this_day =this_date.getDate();
+    this_month = this_date.getMonth()+1;
 
     var activities = [
         /* DAY 1 */
-        'Day 1 text placeholder.',
-    ]
+        "Make hot cocoa.",
+
+        /* DAY 2 */
+        "Sing a carol.",
+
+        /* DAY 3 */
+        "Make paper snowflakes.",
+
+        /* DAY 4 */
+        "Reach out to someone you haven't spoken to in a while.",
+
+        /* DAY 5 */
+        "Watch a Christmas movie.",
+
+        /* DAY6 */
+        "Play a board or card game.",
+
+        /* DAY 7 */
+        "Write a letter to your future self as the ghost of Christmas past.",
+
+        /* DAY 8 */
+        "Pick a Christmas song lyric signature phrase.",
+
+        /* DAY 9 */
+        "Share a fun photo from holiday seasons past.",
+
+        /* DAY 10 */
+        "Eat dinner by candlelight. (Happy Chanukah!)",
+
+        /* DAY 11 */
+        "Research charitable donations or volunteering.",
+
+        /* DAY 12 */
+        "Create a festive cocktail.",
+
+        /* DAY 13 */
+        "Bake cookies.",
+
+        /* DAY 14 */
+        "Research future trips.",
+
+        /* DAY 15 */
+        "Decorate an unexpected spot.",
+
+        /* DAY 16 */
+        "Take a festive photo.",
+
+        /* DAY 17 */
+        "Write a Christmas haiku.",
+
+        /* DAY 18 */
+        "Watch a Christmas movie.",
+
+        /* DAY 19 */
+        "Build a gingerbread house.",
+
+        /* DAY 20 */
+        "Put together a festive outfit.",
+
+        /* DAY 21 */
+        "Turn off the lights and make a flashlight blanket fort. (Happy solstice!)",
+
+        /* DAY 22 */
+        "Send a message of gratitude.",
+
+        /* DAY 23 */
+        "Eat breakfast for dinner.",
+
+        /* DAY 24 */
+        "Make a list of 21 things to do in 2021.",
+        
+        /* DAY 25 */
+        "Merry Christmas!",
+    ];
+
+    var visible = [5, 6, 10, 12, 13, 18, 19, 23, 25];
+
+    /* MAKE SOME DAYS VISIBLE */
+    for (i in visible) {
+        iDay = visible[i];
+        day_text = '#day' + (iDay) + '_text';
+        day_img = '#day' + (iDay) + '_img';
+
+        $(day_text).text(activities[iDay-1]);
+        $(day_img).attr('src','images/orb_visible.png');
+    }
 
     if (this_month == 12) {
-        for (iDay = 0; iDay < this_day; iDay++) {
-            day_text = '#day' + (iDay+1) + '_text'
-            day_img = '#day' + (iDay+1) + '_img'
-            $(day_text).text(activities[iDay])
-            $(day_img).attr('src','images/day' + (iDay+1) + '.png')
+        
+        for (iDay = 0; iDay < 25; iDay++) {
+                day_text = '#day' + (iDay+1) + '_text';
+                day_img = '#day' + (iDay+1) + '_img';
+
+                if (iDay < this_day) {
+                    $(day_text).text(activities[iDay]);
+                    $(day_img).attr('src','images/day' + (iDay+1) + '.png');
+                }
+
         }
+
     }
 
 }
